2b46185e20507c465d0b29148f52a0c38c3fdc33
Stefano <stefe68@gmail.com>
2023-11-29 21:34:53 +0100

    Fix components code

--- src/components/TheFooter.jsx
+++ src/components/TheFooter.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-const TheFooter = () => {
+function TheFooter() {
   return (
-    <footer className="fixed bottom-0 flex h-[8vh] w-full items-center justify-center bg-gray-900 text-gray-100">
-      <p>
-        &copy; {new Date().getFullYear()} Blog Armonie Diverse. Tutti i diritti
-        riservati. Creato con 💀 da Stefano.
-      </p>
-    </footer>
+    <>
+      <footer className="fixed bottom-0 flex h-[8vh] w-full items-center justify-center bg-gray-900 text-gray-100">
+        <p>
+          &copy; {new Date().getFullYear()} Blog Armonie Diverse. Tutti i
+          diritti riservati. Creato con 💀 da Stefano.
+        </p>
+      </footer>
+    </>
   );
-};
+}
 
 export default TheFooter;
