e56ed931ec8b10c3c3480a2431cee6936d6fecd9
Stefano <stefe68@gmail.com>
2023-11-29 15:41:26 +0100

    Add main title in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import {useState} from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
-import Card from "./components/Card";
+import Card from './components/Card';
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	return (
-		<>
-			<Card />
-		</>
-	);
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="mb-10 mt-6 text-4xl font-bold">Un blog per ricordare</h1>
+
+        {/* Componente "Card" */}
+        <Card />
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,28 +3,25 @@ import postImage from '../assets/imgs/one.jpg';
 
 export const Card = () => {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="max-w-lg overflow-hidden rounded-xl bg-gray-300 shadow-lg">
+    <div className="flex items-center justify-center">
+      <div className="max-w-xl overflow-hidden rounded-xl bg-gray-300 shadow-lg">
         <img className="w-full" src={postImage} alt="Contenuto del post" />
         <div className="p-6">
           <div className="mb-2 text-center text-2xl font-bold uppercase text-gray-900">
             Siamo uno, ma diversi
           </div>
           <p className="text-base text-gray-700">
-            Gli indiani nativi americani usavano cacciare i bufali spaventandoli
-            in modo da spingerli verso un dirupo, da cui sarebbero precipitati,
-            morendo. Allo stesso modo l’essere umano fugge dalle paure che non
-            capisce, e lo fa senza sapere in quale direzione sta andando e cosa
-            succederà alla fine della sua fuga. Invece di capire e di affrontare
-            l’ignoto, corre verso la sua distruzione. Le disuguaglianze, di
-            qualunque genere esse siano, sono una fra le maggiori paure che
-            l’uomo sperimenta e che, invece di capire o per lo meno accettare,
-            lo spingono su una strada estremamente pericolosa. Questo è il senso
-            di “One”, una canzone mai capita.
+            I nativi americani usavano cacciare i bufali spaventandoli in modo
+            da spingerli verso un dirupo. Allo stesso modo l’essere umano fugge
+            da ciò che non capisce: invece di affrontare l’ignoto, corre verso
+            la sua distruzione. La diversità, di qualunque genere, è fra le
+            maggiori paure che l’uomo sperimenta e che lo spinge su una strada
+            estremamente pericolosa. Questo è il senso di “One”, una canzone mai
+            capita.
           </p>
-          <p className="mt-3 text-base italic text-gray-900">
-            «Siamo la stessa cosa ma non siamo uguali, e abbiamo la possibilità
-            di sostenerci l’un l’altro. Come una cosa sola».
+          <p className="mt-2 text-base italic text-gray-900">
+            «Siamo la stessa cosa ma non siamo uguali... abbiamo la possibilità
+            di sostenerci l’un l’altro... come una cosa sola».
           </p>
         </div>
         <div className="flex justify-center pb-4">
